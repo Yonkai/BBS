af5fb5f9e84fc0e8aa21ee334f576f90fbfc13ae
Frank <fgunsch.webdev@gmail.com>
2019-02-16 22:23:52 -0500

    Added unrefined/materialized individualboard component

--- components/Boards.js
+++ components/Boards.js
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 const Board = () => (
     <div>
-        <Link href="/tempBoard">
+        <Link href="/indivdualboard">
           <a className="boardtab">Board 1</a>
         </Link>
-        <Link href="/tempBoard">
+        <Link href="/indivdualboard">
           <a className="boardtab">Board 2</a>
         </Link>
     </div>
---
+++ pages/indivdualboard.js
@@ -0,0 +1,9 @@
+import IndividualBoard from '../components/IndividualBoard.js';
+
+
+export default () => (
+    // React Fragment
+    <>
+      <IndividualBoard/>
+    </>
+  )
