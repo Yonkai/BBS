eda72ded7be3868dd301deec4fb734afc199d4ef
Frank <fgunsch.webdev@gmail.com>
2019-02-16 22:17:06 -0500

    Added materialUI icons, added more components, modified notes

--- components/Boards.js
+++ components/Boards.js
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 const Board = () => (
     <div>
-        <Link href="/board1">
+        <Link href="/tempBoard">
           <a className="boardtab">Board 1</a>
         </Link>
-        <Link href="/board2">
+        <Link href="/tempBoard">
           <a className="boardtab">Board 2</a>
         </Link>
     </div>
---
+++ components/IndividualBoard.js
@@ -0,0 +1,10 @@
+import Header from '../components/Header.js';
+
+const IndividualBoard = () => (
+<>
+    <Header/>
+    <p>Indivdual board content</p>
+</>
+)
+
+export default IndividualBoard
