00cd08a8f0f96452e020d15e15ca0631e4f7d8fa
Frank <fgunsch.webdev@gmail.com>
2019-02-26 00:35:17 -0500

    Added more 'content' to boards for TODO

--- components/BoardContent.js
+++ components/BoardContent.js
@@ -5,12 +5,16 @@ import Link from 'next/link';
 const BoardContent = withRouter((props) => (
     <>
         <p className={props.router.query.title}>{`Welcome to ${props.router.query.title}`}</p>
+        <p>Refresh</p>
+        <p>Bottom</p>
         <p>Start New Discussion</p>
         <p>Post 1 + replies</p>
         <p>Post 2 + replies</p>
         <p>Post 3 etc...</p>
         <p>..Post 10 (Bread crumbs to sub pages for this board exclusively)</p>
         <Navigator/>
+        <p>Refresh</p>
+        <p>Bottom</p>
         <Link href="/settings">
             <a>Settings</a>   
         </Link>
