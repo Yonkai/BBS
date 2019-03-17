ce3953688a56a3bd4419bb602ff841de271a0a93
Frank <fgunsch.webdev@gmail.com>
2019-03-16 21:02:22 -0400

    Refined NewThreadButton into an actual button and react class component

--- components/NewThreadButton.js
+++ components/NewThreadButton.js
@@ -1,12 +1,32 @@
-const NewThreadButton = (props) => (
-    <>
-        <a href="#">New Thread</a>
-        <style jsx>{`
-            a{
-                margin-right:15px;
-            }
-        `}</style>
-    </>
-)
+import React from 'react';
+
+class NewThreadButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { content: {} };
+        this.openNewThreadForm = this.openNewThreadForm.bind(this);
+    }
+
+    openNewThreadForm(e){
+        e.preventDefault();
+        console.log('open new thread form function.');
+    }
+
+    render(){
+        return (
+            <>
+                <button onClick={this.openNewThreadForm}>
+                    New Thread
+                </button>
+                <style jsx>{`
+                    button{
+                        margin-right:15px;
+                        margin-bottom:5px;
+                    }
+                `}</style>
+            </>
+        );
+    }
+}
 
 export default NewThreadButton
