a33ae3a5e1e3e1645b9dec07965c8aaef14dbb54
Frank <fgunsch.webdev@gmail.com>
2019-03-17 23:20:58 -0400

    Added starter code for switching between an open and closed form  to create a new thread

--- components/NewThreadButton.js
+++ components/NewThreadButton.js
@@ -3,27 +3,29 @@ import React from 'react';
 class NewThreadButton extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { content: {} };
+        this.state = {openStatus: false};
         this.openNewThreadForm = this.openNewThreadForm.bind(this);
     }
 
     openNewThreadForm(e){
         e.preventDefault();
-        console.log('open new thread form function.');
+        this.setState((state, props) => ({
+            openStatus: !state.openStatus
+          }));
     }
-
+    
     render(){
+        var form;
+        if(!this.state.openStatus){
+            form=<button className="check" onClick={this.openNewThreadForm}>New Thread</button>
+        }else{
+            form=<button onClick={this.openNewThreadForm}>Post Form</button>
+        }
+
         return (
             <>
-                <button onClick={this.openNewThreadForm}>
-                    New Thread
-                </button>
-                <style jsx>{`
-                    button{
-                        margin-right:15px;
-                        margin-bottom:5px;
-                    }
-                `}</style>
+                {form}
+                <style jsx>{`button { margin-right:15px; }`}</style>
             </>
         );
     }
