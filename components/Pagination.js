53566c47b8df76d6c96966dd60875d7e4affc80a
Frank <fgunsch.webdev@gmail.com>
2019-03-25 14:01:18 -0400

    Removed text selection ability from paginator component

--- components/Pagination.js
+++ components/Pagination.js
@@ -144,6 +144,15 @@ class Pagination extends React.Component {
             li:nth-child(${this.state.pager.currentPage+2}){
                 background-color:red;
             }
+            li a{
+                -webkit-touch-callout: none; /* iOS Safari */
+                -webkit-user-select: none; /* Safari */
+                -khtml-user-select: none; /* Konqueror HTML */
+                -moz-user-select: none; /* Firefox */
+                    -ms-user-select: none; /* Internet Explorer/Edge */
+                        user-select: none; /* Non-prefixed version, currently
+                                            supported by Chrome and Opera */
+            }
             `}</style>
         </ul>
         );
