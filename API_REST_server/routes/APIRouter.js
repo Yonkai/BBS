7cc63381225567fa2edadde060f4341b162acde0
Frank <fgunsch.webdev@gmail.com>
2019-04-05 23:40:07 -0400

    Updated routes

--- API_REST_server/routes/APIRouter.js
+++ API_REST_server/routes/APIRouter.js
@@ -16,32 +16,41 @@ router.post('/threadpost', function(req, res, next) {
     res.json(req.body);
 });
 
+router.post('/replytothread', function(req, res, next) {
+    console.log(req.body);
+    //TODO:Implement pooling prod.
+    //https://stackoverflow.com/questions/14087924/cannot-enqueue-handshake-after-invoking-quit
+    
+    //test code
+    hidden_connection.query('INSERT INTO boards (board_name) VALUES (?)',req.body.name, function (error, results, fields) {
+      if (error) throw error;
+    });
+
+    res.json(req.body);
+});
+
+
 //Readers
-router.post('/fillboard', function(req, res, next) {
+router.post('/populateboardfromdatabase', function(req, res, next) {
     console.log(req.body);
     
     //test code
-    hidden_connection.connect();
     hidden_connection.query('SELECT 222 + 333 AS solution', function (error, results, fields) {
       if (error) throw error;
       console.log('The solution is: ', results[0].solution);
     });
-    hidden_connection.end();
 
     res.json(req.body);
 });
 
 //Updaters
-router.post('/editpost', function(req, res, next) {
+router.post('/editreply', function(req, res, next) {
     console.log(req.body);
-    
-    //test code
-    hidden_connection.connect();
+
     hidden_connection.query('SELECT 222 + 333 AS solution', function (error, results, fields) {
       if (error) throw error;
       console.log('The solution is: ', results[0].solution);
     });
-    hidden_connection.end();
 
     res.json(req.body);
 });
@@ -49,14 +58,22 @@ router.post('/editpost', function(req, res, next) {
 //Deleters
 router.post('/deletethread', function(req, res, next) {
     console.log(req.body);
-    
-    //test code
-    hidden_connection.connect();
+
     hidden_connection.query('SELECT 222 + 333 AS solution', function (error, results, fields) {
       if (error) throw error;
       console.log('The solution is: ', results[0].solution);
     });
-    hidden_connection.end();
+
+    res.json(req.body);
+});
+
+router.post('/deletereply', function(req, res, next) {
+    console.log(req.body);
+
+    hidden_connection.query('SELECT 222 + 333 AS solution', function (error, results, fields) {
+      if (error) throw error;
+      console.log('The solution is: ', results[0].solution);
+    });
 
     res.json(req.body);
 });
