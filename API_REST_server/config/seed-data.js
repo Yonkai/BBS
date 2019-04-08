a0522d0854e42c48528a74e9ea6c71aefefed017
Frank <fgunsch.webdev@gmail.com>
2019-04-07 20:05:00 -0400

    Added reply counter based that is based generated  seed data

--- API_REST_server/config/seed-data.js
+++ API_REST_server/config/seed-data.js
@@ -18,23 +18,31 @@ var faker = require('faker');
 function seedGenerator(){
     var seedData = {seedReplys:{},seedThreads:{}};
     var threadCount = 100; //Maximum value:100
-    var replyCount = 1337; //Limited by SQL INT Data type.
-    //Thread seeder:
-    for(i=0;i<100;i++){
+    
+    //Generate initial threads:
+    for(i=0;i<threadCount;i++){
         // let reply = {reply_in_thread_pos: 1, reply_original: 1, reply_username: 'frank',reply_comment:'hello world',thread_owner:1};
         // let thread = {reply_count: 22, subject: 'Hello', board_position:2, board_owner:1};
         //seedData.seedReplys[i] = {reply_in_thread_pos: 1, reply_original: 1, reply_username: 'frank',reply_comment:'hello world',thread_owner:1};
-        seedData.seedThreads[i] = {reply_count: 22, subject: 'Hello', board_position:2, board_owner:1};
-    }
-
-    //Reply seeder:
-    for(i=0;i<3000;i++){
-        // let reply = {reply_in_thread_pos: 1, reply_original: 1, reply_username: 'frank',reply_comment:'hello world',thread_owner:1};
-        // let thread = {reply_count: 22, subject: 'Hello', board_position:2, board_owner:1};
-        seedData.seedReplys[i] = {reply_in_thread_pos: 1, reply_original: 1, reply_username: 'frank',reply_comment:'hello world',thread_owner:1};
-        //seedData.seedThreads[i] = {reply_count: 22, subject: 'Hello', board_position:2, board_owner:1};
+        seedData.seedThreads[i] = {reply_count: faker.random.number(20), subject: faker.random.word(25), board_position:i, board_owner:faker.random.number(7) + 1};
     }
     console.log(seedData);
+
+    //Generate replys to those initial threads that were just generated:
+    var replyCount=0;
+    for (var key in seedData.seedThreads) {
+        replyCount += seedData.seedThreads[key].reply_count;
+        console.log(replyCount);
+      }
+
+
+    //for(i=0;i<replyCount;i++){
+        // let reply = {reply_in_thread_pos: 1, reply_original: 1, reply_username: 'frank',reply_comment:'hello world',thread_owner:1};
+        // let thread = {reply_count: 22, subject: 'Hello', board_position:2, board_owner:1};
+        //seedData.seedReplys[i] = {reply_in_thread_pos: i, reply_original: 0, reply_username:faker.random.word(10),reply_comment:faker.lorem.sentences(100),thread_owner:faker.random.number(99)+1};
+        //seedData.seedThreads[i] = {reply_count: 22, subject: 'Hello', board_position:2, board_owner:1};
+   // }
+
 }
 
 seedGenerator();
