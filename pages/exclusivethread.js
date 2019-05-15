484a8b1e10321ae61861dec41cca77818effeac6
Frank <fgunsch.webdev@gmail.com>
2019-05-14 21:27:09 -0400

    Started optimisitic UX for exclusive threads by making the page for exclusive threads stateful

--- pages/exclusivethread.js
+++ pages/exclusivethread.js
@@ -1,5 +1,4 @@
 //Modules
-import faker from 'faker';
 import ThreadPreviewRootComponent from '../components/Threads/ThreadPreviewMobile/ThreadPreviewRootComponent.js'; 
 import Head from 'next/head';
 import React from 'react';
@@ -11,58 +10,130 @@ import SubReplyRootComponent from '../components/Threads/ReplyComponentsMobile/S
 import axios from 'axios';
 import {withRouter} from 'next/router';
 
-const exclusivethread = withRouter((props) => (
-    // React Fragment
-    <>
-      <Head>
-        <title>{props.router.query.pathname}</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no">
-        </meta>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-      </Head>
-      <Navigator boards={props.boards}/>
+class ExclusiveThreadPage extends React.Component{
+  constructor(props){
+    super(props);
+      this.handleOptimisticReplys = this.handleOptimisticReplys.bind(this);
 
-        <h3>Placeholder exclusive thread page. thread id:{props.router.query.threadID}</h3>
-        <Header/>
-        <NewThreadButton isThisPartOfAnExclusiveThread={true} threadID={props.router.query.threadID} boardID={props.router.query.boardID}/>
-        {/* Data needs to be loaded here again: */}
-        {/*Map out data here form database call*/}
-        <ThreadPreviewRootComponent indivdualThreadData={props.exclusiveThread} replyCount={props.replyCount} router={props.router}/>
-        {/* For exlcusive threads only, map props of replys for the exlcusive page*/}
-        <SubReplyRootComponent individualReplyData={props.exclusiveThreadReplys} replySubject={props.exclusiveThread[0].threads_subject}/>
-        <Footer/>
-
-      <style jsx global>{`
-      html {
-        box-sizing:border-box;
-        margin:0; 
+      this.state = {
+        //Optimistic UX displays reply before it is actually inserted into the database 
+        //for a more salient experience.
+        optimisticComment:[],
+        optimisiticUsername:'',
+        optimisiticTimestamp:0
       }
+  }
 
-      body{
-        margin:0;
-        min-height:100vh;
-        background: DimGrey;  /* fallback for old browsers */
-        // background: -webkit-linear-gradient(to bottom, #FFFFFF, #FFEFBA);  /* Chrome 10-25, Safari 5.1-6 */
-        // background: linear-gradient(to bottom, #FFFFFF, #FFEFBA); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-        // background-size: cover;
-      }
-    `}</style>
-    </>
-  ))
+  handleOptimisticReplys(){
+    //Sets the optimistic UX state from NewThreadButton component.
+    console.log('Hello world');
+  }
 
-//2.(And also) pull in database props/tables/columns here...
-//Pull replys/threads and load into app:
-//See axios npm docs for response schema.
-//Can only use getInitialProps on NextJS pages files
-//Need absolute path unless using a baseurl in axios
-exclusivethread.getInitialProps =  async(context) => {
-  console.log('context object: ',context);
+  render(){
+    return(
+      <>
+        <Head>
+          <title>{this.props.router.query.pathname}</title>
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no">
+          </meta>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+        </Head>
+        <Navigator boards={this.props.boards}/>
+
+          <h3>Placeholder exclusive thread page. thread id:{this.props.router.query.threadID}</h3>
+          <Header/>
+          <NewThreadButton 
+          isThisPartOfAnExclusiveThread={true} 
+          threadID={this.props.router.query.threadID} 
+          boardID={this.props.router.query.boardID}
+          />
+          {/* Data needs to be loaded here again: */}
+          {/*Map out data here form database call*/}
+          <ThreadPreviewRootComponent 
+          indivdualThreadData={this.props.exclusiveThread} 
+          replyCount={this.props.replyCount} 
+          router={this.props.router}
+          />
+          {/* For exlcusive threads only, map props of replys for the exlcusive page*/}
+          <SubReplyRootComponent 
+          individualReplyData={this.props.exclusiveThreadReplys} 
+          replySubject={this.props.exclusiveThread[0].threads_subject}
+          />
+          <Footer/>
+
+        <style jsx global>{`
+        html {
+          box-sizing:border-box;
+          margin:0; 
+        }
+
+        body{
+          margin:0;
+          min-height:100vh;
+          background: DimGrey;  /* fallback for old browsers */
+          // background: -webkit-linear-gradient(to bottom, #FFFFFF, #FFEFBA);  /* Chrome 10-25, Safari 5.1-6 */
+          // background: linear-gradient(to bottom, #FFFFFF, #FFEFBA); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+          // background-size: cover;
+        }
+      `}</style>
+      </>
+    );
+  }
+}
+
+// const exclusivethread = withRouter((props) => (
+//     // React Fragment
+//     <>
+//       <Head>
+//         <title>{props.router.query.pathname}</title>
+//         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no">
+//         </meta>
+//         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+//         <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+//       </Head>
+//       <Navigator boards={props.boards}/>
+
+//         <h3>Placeholder exclusive thread page. thread id:{props.router.query.threadID}</h3>
+//         <Header/>
+//         <NewThreadButton isThisPartOfAnExclusiveThread={true} threadID={props.router.query.threadID} boardID={props.router.query.boardID}/>
+//         {/* Data needs to be loaded here again: */}
+//         {/*Map out data here form database call*/}
+//         <ThreadPreviewRootComponent indivdualThreadData={props.exclusiveThread} replyCount={props.replyCount} router={props.router}/>
+//         {/* For exlcusive threads only, map props of replys for the exlcusive page*/}
+//         <SubReplyRootComponent individualReplyData={props.exclusiveThreadReplys} replySubject={props.exclusiveThread[0].threads_subject}/>
+//         <Footer/>
+
+//       <style jsx global>{`
+//       html {
+//         box-sizing:border-box;
+//         margin:0; 
+//       }
+
+//       body{
+//         margin:0;
+//         min-height:100vh;
+//         background: DimGrey;  /* fallback for old browsers */
+//         // background: -webkit-linear-gradient(to bottom, #FFFFFF, #FFEFBA);  /* Chrome 10-25, Safari 5.1-6 */
+//         // background: linear-gradient(to bottom, #FFFFFF, #FFEFBA); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+//         // background-size: cover;
+//       }
+//     `}</style>
+//     </>
+//   ))
+
+// //2.(And also) pull in database props/tables/columns here...
+// //Pull replys/threads and load into app:
+// //See axios npm docs for response schema.
+// //Can only use getInitialProps on NextJS pages files
+// //Need absolute path unless using a baseurl in axios
+ExclusiveThreadPage.getInitialProps =  async(context) => {
+  console.log('context object:',context);
   // const thread_response = await axios.post('http://localhost:4000/api/readthreads');
   // const reply_response = await axios.post('http://localhost:4000/api/readreplys');
   const board_response = await axios.post('http://localhost:4000/api/readboards');
-  const exclusive_thread =  await axios.post(`http://localhost:4000/api/readexclusivethread/${context.query.threadID}`);
-  const exclusive_thread_replys =  await axios.post(`http://localhost:4000/api/readexclusivereplys/${context.query.threadID}`);
+  const exclusive_thread = await axios.post(`http://localhost:4000/api/readexclusivethread/${context.query.threadID}`);
+  const exclusive_thread_replys = await axios.post(`http://localhost:4000/api/readexclusivereplys/${context.query.threadID}`);
   const threads_reply_count = await axios.post('http://localhost:4000/api/readthreadsreplycount');
   console.log(
     // thread_response.data, 
@@ -81,4 +152,4 @@ exclusivethread.getInitialProps =  async(context) => {
   };
 }
 
-export default exclusivethread;
+export default withRouter(ExclusiveThreadPage);
