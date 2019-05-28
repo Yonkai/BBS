c2127f8f5d552451ce01d3dbcfd62eddceeb9d06
Frank <fgunsch.webdev@gmail.com>
2019-05-27 22:12:12 -0400

    Add changes for responsiveness/consistency to exclusive thread page

--- pages/exclusivethread.js
+++ pages/exclusivethread.js
@@ -82,7 +82,7 @@ class ExclusiveThreadPage extends React.Component{
     return(
       <>
         <Head>
-          <title>{this.props.router.query.title}</title>
+          <title>{this.props.router.query.title} {this.props.router.query.threadID}</title>
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"/>
           <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" 
       integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" 
@@ -92,44 +92,46 @@ class ExclusiveThreadPage extends React.Component{
           
         </Head>
         <Navigator boards={this.props.boards}/>
-
-          <h3>Placeholder exclusive thread page. thread id:{this.props.router.query.threadID}</h3>
-          <Header/>
+          <h2>{this.props.router.query.title} {this.props.router.query.threadID}</h2>
+          <div className='header'>
+            <Header/>
+          </div>
           <NewThreadButton 
           isThisPartOfAnExclusiveThread={true} 
           threadID={this.props.router.query.threadID} 
           boardID={this.props.router.query.boardID}
           handleOptimisticReplys={this.handleOptimisticReplys}
           />
-          {/* Data needs to be loaded here again: */}
-          {/*Map out data here form database call*/}
-          <ThreadPreviewRootComponent 
-          indivdualThreadData={this.props.exclusiveThread} 
-          replyCount={this.props.replyCount} 
-          router={this.props.router}
-          />
-          {/* For exlcusive threads only, map props of replys for the exlcusive page*/}
-          {this.state.polledExclusiveReplys.length>this.props.exclusiveThreadReplys.length?
-          <SubReplyRootComponent 
-          individualReplyData={this.state.polledExclusiveReplys} 
-          replySubject={this.props.exclusiveThread[0].threads_subject}
-          />:
-          <SubReplyRootComponent 
-          individualReplyData={this.props.exclusiveThreadReplys} 
-          replySubject={this.props.exclusiveThread[0].threads_subject}
-          />
+          <div className='container'>
+            {/* Data needs to be loaded here again: */}
+            {/*Map out data here form database call*/}
+            <ThreadPreviewRootComponent 
+            indivdualThreadData={this.props.exclusiveThread} 
+            replyCount={this.props.replyCount} 
+            router={this.props.router}
+            />
+            {/* For exlcusive threads only, map props of replys for the exlcusive page*/}
+            {this.state.polledExclusiveReplys.length>this.props.exclusiveThreadReplys.length?
+            <SubReplyRootComponent 
+            individualReplyData={this.state.polledExclusiveReplys} 
+            replySubject={this.props.exclusiveThread[0].threads_subject}
+            />:
+            <SubReplyRootComponent 
+            individualReplyData={this.props.exclusiveThreadReplys} 
+            replySubject={this.props.exclusiveThread[0].threads_subject}
+            />
 
-          }
-          {this.state.optimisiticFlag?
-          this.state.optimisticComment.map((comments,index) =>
-            <OptimisticSubReplyRootComponent
-             replyUsername={this.state.optimisiticUsername[index]}
-             replyComment={comments}
-             replySubject={this.props.exclusiveThread[0].threads_subject}
-             threadTime={new Date()}
-             threadID={123456}
-           />):<span></span>
-           }
+            }
+            {this.state.optimisiticFlag?
+            this.state.optimisticComment.map((comments,index) =>
+              <OptimisticSubReplyRootComponent
+                replyUsername={this.state.optimisiticUsername[index]}
+                replyComment={comments}
+                replySubject={this.props.exclusiveThread[0].threads_subject}
+                threadTime={new Date()}
+                threadID={123456}
+              />):<span></span>}
+            </div>
           <Footer/>
 
         <style jsx global>{`
@@ -141,11 +143,24 @@ class ExclusiveThreadPage extends React.Component{
         body{
           margin:0;
           min-height:100vh;
-          background: DimGrey;  /* fallback for old browsers */
+          background: #cce;  /* fallback for old browsers */
           // background: -webkit-linear-gradient(to bottom, #FFFFFF, #FFEFBA);  /* Chrome 10-25, Safari 5.1-6 */
           // background: linear-gradient(to bottom, #FFFFFF, #FFEFBA); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
           // background-size: cover;
         }
+
+        .header{
+            display:grid;
+            justify-items: center;
+            grid-gap:5px;
+        }
+
+          .container{
+            display:grid;
+            justify-items:center;
+            grid-template-columns:1fr;
+            background:#cce;  /* fallback for old browsers */
+          }
       `}</style>
       </>
     );
