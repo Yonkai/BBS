2722e6feb77b46931d19189a5686fb67cb26deb5
Frank <fgunsch.webdev@gmail.com>
2019-06-07 23:20:06 -0400

    Add note

--- API_REST_server/config/db.js
+++ API_REST_server/config/db.js
@@ -1,4 +1,5 @@
 //every call to require('./db') will get exactly the same object returned, if it would resolve to the same file.
+//Make sure that a MySQL user is properly configured on your server.
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : '127.0.0.1',
