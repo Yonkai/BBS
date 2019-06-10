d28c39401251c51f70e65cd52acad62e9acf1d0d
Frank <fgunsch.webdev@gmail.com>
2019-06-09 20:49:55 -0400

    Add field to frontend config object

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
 var config = {
-    repollInterval:30000
+    repollInterval:30000,
+    //different on prod. obviously
+    serverRESTfulConnectionPrefix:'http://localhost:4000'
   }
 
 export default config;
